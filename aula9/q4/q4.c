3a6632ffa7b93658c94c5cf596026478eaf4ce67
MarcosFelipeC <felipe-marcos10@hotmail.com>
2021-05-03 11:01:44 -0300

    q6 alterada

--- aula9/q4/q4.c
+++ aula9/q4/q4.c
@@ -4,7 +4,7 @@ int main(){
 	denominadorFinal = 50;
 	float resultadoSoma = 0;
 	const int RAZAONUMERADOR = 2, RAZAODENOMINADOR = 1;
-	for (int contador = 0; contador<=denominadorFinal; contador++){
+	for (int contador = 0; contador< denominadorFinal; contador++){
 		resultadoSoma += (float)(numeradorInicial+contador*RAZAONUMERADOR)/
 		(float)(denominadorInicial+contador*RAZAODENOMINADOR);
 	}
--- aula9/q6/q6.c
+++ aula9/q6/q6.c
@@ -4,9 +4,11 @@ int main(){
         int numerador = 4, denominadorInicial = 1, denominadorFinal = 29;
         float resultadoSoma = 0;
         const int RAZAODENOMINADOR = 2;
-        for (int contador = 0; contador<=denominadorFinal; contador+=RAZAODENOMINADOR){
+        for (int contador = 0; contador<denominadorFinal; contador+=RAZAODENOMINADOR){
                 resultadoSoma += (float)numerador/(float)(denominadorInicial+contador);
 		numerador*=(-1);
+		printf("\nContador: %i, ",contador);
+		printf("Soma: %f\n", resultadoSoma);
 	}
         printf("O resultado de pi é: %f",resultadoSoma);
 }
