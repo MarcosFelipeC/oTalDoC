a9ca085a8727ee39168822b65028517f8eda8abd
MarcosFelipeC <felipe-marcos10@hotmail.com>
2021-05-03 13:59:00 -0300

    Alteracoes pontuais

--- aula9/q5/q5.c
+++ aula9/q5/q5.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 int main(){
-	int numeradorInicial = 37, denominadorInicial = 2, numeradorFinal = 1,
+	int numeradorInicial = 37, denominadorInicial = 1, numeradorFinal = 1,
 	denominadorFinal = 29;
 	float resultadoSoma = 0;
 	const int RAZAODENOMINADOR = 1;
-	for (int contador = 0; contador<=denominadorFinal; contador++)
-		resultadoSoma += (float)((numeradorInicial-contador)*(numeradorInicial-(contador+1)))/
+
+	for (int contador = 0; contador<denominadorFinal; contador++){
+		resultadoSoma += (float)((numeradorInicial-contador)*(numeradorInicial-(contador-1)))/
 		(float)(denominadorInicial+contador*RAZAODENOMINADOR);
+		printf("\nContador: %i, ",contador);
+                printf("Soma: %f\n", resultadoSoma);
+
+	}
 
 	printf("O resultado de M é: %f",resultadoSoma);
 }
