25bb599ef7c8ad98c8b4150f8f1ef9ccf3446228
Marcos_Felipe <felipe-marcos10@hotmail.com>
2021-05-05 10:34:08 -0300

    adicao da verificacao de A

--- aula9/q1/q1.c
+++ aula9/q1/q1.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <math.h>
-void main(){
+int main(){
 	float a,b,c,delta;
 	float x1,x2;
 	printf("Digite o valor de a,b,c:\n");
 	scanf("%f,%f,%f",&a,&b,&c);
+	if (a==0){
+		printf("\nO coeficinete angular deve ser diferente de zero!\n");
+		return(99);
+	}
 	delta = (b*b - 4*(a*c));
 	float raiz_delta;
 	if (delta >= 0){
@@ -15,4 +19,5 @@ void main(){
 		else printf("\nx1=x2=%f\n", x1);
 	}
 	else printf("\nO número não possui raizes reais\n");
+	return(0);
 }
